607ce22a19adbd38d485d5c5cb7546567cb50c63
vbalaji215-dev <vbalaji215@outlook.com>
2022-02-03 05:49:03 -0500

    Added ZeusApiResponse and Constants classes

---
+++ src/main/java/com/brihaspathee/zeus/constants/ApiResponseConstants.java
@@ -0,0 +1,16 @@
+package com.brihaspathee.zeus.constants;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 03, February 2022
+ * Time: 5:46 AM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.constants
+ * To change this template use File | Settings | File and Code Template
+ */
+public class ApiResponseConstants {
+
+    public static final String SUCCESS = "Success";
+    public static final String FAIL = "Fail";
+}
