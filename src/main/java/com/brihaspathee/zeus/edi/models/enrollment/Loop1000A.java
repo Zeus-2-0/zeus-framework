9ccb7fe7529473e82790c557da0471564a48daff
vbalaji215-dev <vbalaji215@outlook.com>
2022-02-22 15:23:51 -0500

    ZEUS-13 added the classes for segments of 834

---
+++ src/main/java/com/brihaspathee/zeus/edi/models/enrollment/Loop1000A.java
@@ -0,0 +1,13 @@
+package com.brihaspathee.zeus.edi.models.enrollment;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 21, February 2022
+ * Time: 9:06 PM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.edi.models.enrollment
+ * To change this template use File | Settings | File and Code Template
+ */
+public class Loop1000A {
+}
